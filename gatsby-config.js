091b4398c2bd179b40f82ae1a6c64ec13e09fcf3
Allex372 <alexalex372.aa@gmail.com>
2023-05-19 12:11:00 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `graff.1.0`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}

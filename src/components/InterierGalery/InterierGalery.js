54994e41c9f0f67eb6e1d1447609ffcdc9639718
Allex372 <alexalex372.aa@gmail.com>
2023-06-15 18:20:14 +0300

    fix styles

--- src/components/InterierGalery/InterierGalery.js
+++ src/components/InterierGalery/InterierGalery.js
@@ -24,8 +24,8 @@ const InterierGalery = () => {
 
     const fetchFetchData = async () => {
         try {
-            const token = '571ed7986473215e45d999825cca0187c80a0561cf3246a38e6aa437a408bda689eacd923909bdebf75b17ae696f35208f876bc02fb3e7479f20c89e4d3711e66bfa7795d9b8a2f8122f4d8f9a63e2d306259f4048eb8048a7d27fadfde000139d1909b1abd9bde84980162a303658a65e18346446b6e5c19e42631ec1a6aeaa';
-            const response = await fetch('https://whispering-shore-87525.herokuapp.com/api/interiers?populate=*', {
+            const token = '24da86087f116291ec96ad43dab23b303ab9953b419db27224d705a0241d95806bf312d2019a0b39046444ea84e06fcfcfb764fdc28ec15d5a40f24368c343ea99c9c413aa61aa28cd472da495c88e21486f4769d9199cb861a55ef9b89af31d80ee1e8983e6adb6e0b05ab2b45bd9d56b617d96d16b69a7ca9bd059e17d5f27';
+            const response = await fetch('https://vast-fjord-05237.herokuapp.com/api/interiers?populate=*', {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
